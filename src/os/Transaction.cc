f228a6ff488b97b429fed0fdc3066dc436c71e78
ppomee2002 <ppomee20024@naver.com>
2026-08-18 03:17:35 +0000

    os: add versioned transaction payload for vector entries
    Add an OP_PUT_VECTOR_NODE Transaction op carrying a versioned vector
    entry payload, so a vector PUT can be journaled and replayed like any
    other op. cyanstore rejects the new op explicitly since it doesn't
    support the VectorNode path.

--- src/os/Transaction.cc
+++ src/os/Transaction.cc
@@ -401,21 +401,16 @@ void Transaction::dump(ceph::Formatter *f)
       }
       break;
 
-    case Transaction::OP_PUT_VECTOR:
+    case Transaction::OP_PUT_VECTOR_NODE:
       {
         coll_t cid = i.get_cid(op->cid);
         ghobject_t oid = i.get_oid(op->oid);
-	map<string, bufferlist> aset;
-	i.decode_attrset(aset);
-	f->dump_string("op_name", "put_vector");
+	bufferlist record_bl;
+	i.decode_bl(record_bl);
+	f->dump_string("op_name", "put_vector_node");
 	f->dump_stream("collection") << cid;
 	f->dump_stream("oid") << oid;
-	f->open_object_section("attr_lens");
-	for (map<string, bufferlist>::iterator p = aset.begin();
-	    p != aset.end(); ++p) {
-	  f->dump_unsigned(p->first.c_str(), p->second.length());
-	}
-	f->close_section();
+	f->dump_unsigned("record_length", record_bl.length());
       }
       break;
 
